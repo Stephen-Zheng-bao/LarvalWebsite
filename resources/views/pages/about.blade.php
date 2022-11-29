20cf7be371f6fe0683b1b49504fb5e7e81116f18
YaseenZaman <116076545+YaseenZaman@users.noreply.github.com>
2022-11-29 10:44:10 +0000

    About us page
    Changed about us page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,44 +2,75 @@
 
 
     <!-- TODO: This is filled with the About us page-->
+    <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>About Us</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="app.css">
+ 
+       <script src="https://kit.fontawesome.com/530bb6b399.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        
+        <section>
+            
+            <div class = "image">
+                <!-- image here -->
+            </div>
+
+            <div class = "content">
+
+                <h2>About<span> Us!</span></h2>
+           <p></p>
+               <div>
+                
+                <p> We encourage technological advancements, 
+                and provide high-end tech related products. 
+                Our mission is to be the world largest tech giant on the planet, 
+                and enforce our values of customer commitment, integrity, constant improvement,
+                and environmental friendliness</p>
+
+                <p>Our vision for YazTec is to be recognisable and reliable worldwide,
+                we exposed our brand to the UK and US, 
+                with plans to expand to Australia and the UAE. We design, 
+                develop, and sell electronics such as gaming hardware while also 
+                offering some exclusive services to our loyal members.</p>
+            
+                <div><p>We are determined to improve the state of the world,
+                    by improving the mental state of the people,
+                    Our products are the easiest to implement,
+                    As well as being the most cost effective in the industry.
+                    we will have what you need, when you need and where you need.
+                    Don't ask the how because well do that for you.</p>
+                </div>
+            </div>
+                <div>
+
+                <ul class = "icons">
+                    <li>
+                        <i class="fa-brands fa-linkedin"></i>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-instagram"></i>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-facebook"></i>
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-twitter"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </section>
+    </body>
 
 
-
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('About Us') }}
-        </h2>
-
-
-
-
-
-    <div class = "ourPurpose">
-        <h1>OUR PURPOSE</h1>
-        <p>We are YazTec. A company that encourages technological advancements, </p>
-        <p>and provides high-end tech related products. </p>
-        <p>Our mission is to be the world largest tech giant on the planet, </p>
-        <p>and enforce our values of customer commitment, integrity, constant improvement,</p>
-        <p>and environmental friendliness.</p>
-    </div>
-
-    <div class = "ourVision">
-        <h1>OUR VISION</h1>
-        <p>Our vision for YazTec is to be recognisable and reliable worldwide,</p>
-        <p>we exposed our brand to the UK and US, </p>
-        <p>with plans to expand to Australia and the UAE. We design, </p>
-        <p>develop, and sell electronics such as gaming hardware while also </p>
-        <p>offering some exclusive services to our loyal members.</p>
-    </div>
-
-    <div class = "ourStatement">
-        <h1>OUR STATEMENT</h1>
-        <p>We are determined to improve the state of the world,</p>
-        <p>by improving the mental state of the people, </p>
-        <p>Our products are the easiest to implement,</p>
-        <p>As well as being the most cost effective in the industry.</p>
-        <p>we will have what you need, when you need and where you need.</p>
-        <p>Don't ask the how because well do that for you.</p>
-    </div>
+    
+</html>
 
 
 </x-app-layout>
